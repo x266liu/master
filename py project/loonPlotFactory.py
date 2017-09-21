3ea04df4d26bd36ff644fd142ffadb75ded4fd64
x266liu <x266liu@uwaterloo.ca>
2017-09-20 20:34:09 -0400

    second commit

--- py project/loonPlotFactory.py
+++ py project/loonPlotFactory.py
@@ -1,8 +1,16 @@
 from tkinter import *
 from tkinter import ttk
+import l_toplevel
+import l_subwin
 
-#root = Tk()
+class loonPlotFactory(Frame) :
+    def __init__(self, patent = None):
+        selffactory_tclcmd, factory_path, factory_window_title = "loon plot"
+    
+    
+
+root = Tk()
+root.title("loon plot")
+factory = ttk.Frame(root)
 #ttk.Button(root, text = "Hello World").grid()
-#root.mainloop()
-
-def loonPlotFactory()
+root.mainloop()
