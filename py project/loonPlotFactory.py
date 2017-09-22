f47a59c907d0247a61007f39709c69e925a9c25a
x266liu <x266liu@uwaterloo.ca>
2017-09-21 21:38:22 -0400

    loonfactory

--- py project/loonPlotFactory.py
+++ py project/loonPlotFactory.py
@@ -3,8 +3,11 @@ from tkinter import ttk
 import l_toplevel
 import l_subwin
 
+
+root = Tk()
+
 class loonPlotFactory:
-    def __init__(self, factory_tclcmd, factory_path, factory_window_title="loon plot", parent = None):
+    def __init__(self, factory_tclcmd, factory_path, factory_window_title="loon plot", parent = None, *therest):
         self.factory_tclcmd = factory_tclcmd
         self.factory_path = factory_path
         self.factory_window_title = "loon plot"
@@ -12,12 +15,24 @@ class loonPlotFactory:
     
     def lpf(self):
         new.toplevel = True
+        if parent is None:
+            new.toplevel = False
+            parent = l_toplevel
         
+        child = l_subwin(parent, factory_path)
+        try:
+            plot = root.tk.call(factory_tclcmd, child, *lst)
+        except:   
+            if new.toplevel:
+                parent.destroy()    
+            
+                
+                
         
 #Tk.call() tcl() in r
 
 
-root = Tk()
+#root = Tk()
 root.title("loon plot")
 factory = ttk.Frame(root)
 #ttk.Button(root, text = "Hello World").grid()
